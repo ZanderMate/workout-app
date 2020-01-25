93826b43f3d227b2fb0525a418f9528f404247ff
Alex Griep <alexgriep@hotmail.com>
2020-01-25 16:25:11 -0600

    added first html routes and server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.listen(PORT, () => {
+    console.log("App running on port" , PORT);
+});
